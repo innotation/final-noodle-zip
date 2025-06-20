778d7d6abe438c2e825f532ba1b620a623a5a1b0
seokbong <ssb7779@naver.com>
2025-06-20 12:42:55 +0900

    Setting: 초기세팅

---
+++ src/main/java/noodlezip/NoodleZipApplication.java
@@ -0,0 +1,13 @@
+package noodlezip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoodleZipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoodleZipApplication.class, args);
+	}
+
+}
---
+++ src/test/java/noodlezip/NoodleZipApplicationTests.java
@@ -0,0 +1,13 @@
+package noodlezip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoodleZipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
