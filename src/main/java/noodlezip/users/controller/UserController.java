26d1c6ba50f56fb18ce926ab1af0e00947585032
inhomun <inho8063@icloud.com>
2025-06-23 17:28:35 +0900

    Feat: 스프링 시큐리티 설정 추가

--- src/main/java/noodlezip/users/controller/UserController.java
+++ src/main/java/noodlezip/users/controller/UserController.java
@@ -1,8 +1,10 @@
 package noodlezip.users.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UserController {
-    
+    @GetMapping("/login")
+    public void loginPage(){}
 }
