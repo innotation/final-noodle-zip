27ebc58ddf0ab8dc5d44f8841e07f9d125791768
sotogito <sotogitoarchive@gmail.com>
2025-06-24 20:07:49 +0900

    Feat : BadgeCategory Entity 생성

---
+++ src/main/java/noodlezip/badge/entity/BadgeCategory.java
@@ -0,0 +1,38 @@
+package noodlezip.badge.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Builder
+
+@Entity
+@Table(name = "tbl_badge_category")
+public class BadgeCategory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "badge_category_id", nullable = false)
+    private Long id;
+
+    @Column(name = "badge_category_name", nullable = false, length = 30)
+    private String badgeCategoryName;
+
+    @Column(name = "badge_description", length = 100)
+    private String badgeDescription;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "badge_group_id", nullable = false)
+    private BadgeGroup badgeGroup;
+
+    @OneToMany(mappedBy = "badgeCategory", fetch = FetchType.LAZY)
+    private List<Badge> badgeList;
+
+    @Column(name = "is_event", nullable = false)
+    private Boolean isEvent = false;
+
+}
