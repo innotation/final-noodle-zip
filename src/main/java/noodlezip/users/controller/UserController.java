6ef7c6e6333a8d71e049df59e5d2a59d0ea9c2ce
inhomun <inho8063@icloud.com>
2025-06-24 17:46:16 +0900

    Design: 환경설정 뷰 작업에 필요한 파일 추가 및 권한 설정

--- src/main/java/noodlezip/users/controller/UserController.java
+++ src/main/java/noodlezip/users/controller/UserController.java
@@ -7,4 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class UserController {
     @GetMapping("/login")
     public void loginPage(){}
+
+    @GetMapping("/signup")
+    public void signupPage(){}
 }
