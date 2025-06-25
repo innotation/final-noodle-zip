5862abe3905ae9123a4fb35828007e533bfb7fd3
sotogito <sotogitoarchive@gmail.com>
2025-06-25 17:30:23 +0900

    Feat : 메인 Application @EnableJpaAuditing 어노테이션 추가

--- src/main/java/noodlezip/NoodleZipApplication.java
+++ src/main/java/noodlezip/NoodleZipApplication.java
@@ -1,8 +1,12 @@
 package noodlezip;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class NoodleZipApplication {
 
