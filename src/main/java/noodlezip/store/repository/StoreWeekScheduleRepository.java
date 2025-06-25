0c53d3858dc99d68b022d4b28c26a2e777cf5a0f
Seoyeon <106144694+podoseee@users.noreply.github.com>
2025-06-25 12:11:15 +0900

    Remove: 불필요한 StoreDto, Repository 삭제

--- src/main/java/noodlezip/store/repository/StoreWeekScheduleRepository.java
+++
@@ -1,8 +0,0 @@
-package noodlezip.store.repository;
-
-import noodlezip.store.entity.StoreWeekSchedule;
-import noodlezip.store.entity.StoreWeekScheduleId;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StoreWeekScheduleRepository extends JpaRepository<StoreWeekSchedule, StoreWeekScheduleId> {
-}
