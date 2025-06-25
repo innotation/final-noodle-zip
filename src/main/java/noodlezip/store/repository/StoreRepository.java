3a7fbf893a2f3d9e2b67446c1858115884f029af
Seoyeon <106144694+podoseee@users.noreply.github.com>
2025-06-25 17:50:49 +0900

    Fix: repository에 있는 이름 수정(Tbl 없애기)

--- src/main/java/noodlezip/store/repository/StoreRepository.java
+++ src/main/java/noodlezip/store/repository/StoreRepository.java
@@ -1,7 +1,7 @@
 package noodlezip.store.repository;
 
-import noodlezip.store.entity.TblStore;
+import noodlezip.store.entity.Store;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface StoreRepository extends JpaRepository<TblStore, Long> {
+public interface StoreRepository extends JpaRepository<Store, Long> {
 }
