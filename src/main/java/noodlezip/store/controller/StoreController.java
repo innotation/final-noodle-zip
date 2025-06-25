0d437a265d5848251708e092300bd2da51787e0e
Seoyeon <106144694+podoseee@users.noreply.github.com>
2025-06-25 12:20:39 +0900

    Feature: 매장 등록 Controller

--- src/main/java/noodlezip/store/controller/StoreController.java
+++ src/main/java/noodlezip/store/controller/StoreController.java
@@ -1,24 +1,25 @@
 package noodlezip.store.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import noodlezip.store.dto.StoreRequestDto;
-import noodlezip.store.entity.Store;
 import noodlezip.store.service.StoreService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/stores")
+@Tag(name = "Store API", description = "매장 관련 API")
 @RequiredArgsConstructor
 public class StoreController {
+
     private final StoreService storeService;
 
+    @Operation(summary = "매장 등록", description = "신규 매장을 등록합니다.")
     @PostMapping
-    public ResponseEntity<?> createStore(@RequestBody StoreRequestDto dto) {
-        Store store = storeService.saveStore(dto);
-        return ResponseEntity.ok(store.getStoreId());
+    public ResponseEntity<Long> registerStore(@RequestBody StoreRequestDto dto) {
+        Long storeId = storeService.registerStore(dto);
+        return ResponseEntity.ok(storeId);
     }
 }
