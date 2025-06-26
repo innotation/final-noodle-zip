473c72f0ad680b7e8eaa70d548e5ce71d4a3ad5d
sotogito <sotogitoarchive@gmail.com>
2025-06-26 17:43:25 +0900

    Feat : UserBadgeRepository 생성 및 배지 반환 쿼리 작성

---
+++ src/main/java/noodlezip/badge/repository/UserBadgeRepository.java
@@ -0,0 +1,42 @@
+package noodlezip.badge.repository;
+
+import noodlezip.badge.entity.UserBadge;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface UserBadgeRepository extends JpaRepository<UserBadge, Long> {
+
+    @Query("""
+            select ub
+            from UserBadge ub
+            left join fetch ub.badge b
+            where ub.userId = :userId
+            and b.badgeCategory.id = :badgeCategoryId
+            order by b.badgePolicy.badgeLevel desc
+            """)
+    List<UserBadge> findByUserIdAndBadgeCategoryIdWithBadge(
+            long userId,
+            long badgeCategoryId,
+            Pageable pageable
+    );
+
+
+    @Query("""
+            select ub
+            from UserBadge ub
+            left join fetch ub.badge b
+            where ub.userId = :userId
+            and b.badgeCategory.id = :badgeCategoryId
+            and b.badgeExtraOption.storeSidoLegalCode = :legalCode
+            order by ub.id desc
+            """)
+    List<UserBadge> findByUserIdAndBadgeCategoryIdAndLegalCode(
+            long userId,
+            long badgeCategoryId,
+            int legalCode
+    );
+
+}
