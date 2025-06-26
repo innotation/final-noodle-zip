1d872508303a0cab912a0175e4c3762cc437c5bb
sotogito <sotogitoarchive@gmail.com>
2025-06-26 17:35:14 +0900

    Feat : 배지 카테고리 Enum 정의

---
+++ src/main/java/noodlezip/badge/constants/LevelBadgeCategoryType.java
@@ -0,0 +1,23 @@
+package noodlezip.badge.constants;
+
+import lombok.Getter;
+
+@Getter
+public enum LevelBadgeCategoryType {
+
+    REVIEW_GET_LIKE_COUNT_BADGE(1L),
+    COMMUNITY_GET_LIKE_COUNT_BADGE(2L),
+
+    ALL_COMMUNITY_POST_COUNT_BADGE(3L),
+    ALL_COMMENT_POST_COUNT_BADGE(4L),
+
+    RAMEN_REVIEW_REGION_BADGE(5L),
+    RAMEN_REVIEW_CATEGORY_BADGE(6L);
+
+    private final Long dbPk;
+
+    LevelBadgeCategoryType(Long dbPk) {
+        this.dbPk = dbPk;
+    }
+
+}
