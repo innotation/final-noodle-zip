b1da23f142aa2abe51febb01ac00f29c30f92f4c
sotogito <sotogitoarchive@gmail.com>
2025-06-27 17:52:56 +0900

    Feat : 배지 전략 타입 Enum에 누적타입 boolean 정의

--- src/main/java/noodlezip/badge/constants/BadgeStrategyType.java
+++ src/main/java/noodlezip/badge/constants/BadgeStrategyType.java
@@ -5,18 +5,21 @@ import lombok.Getter;
 @Getter
 public enum BadgeStrategyType {
 
-    SINGLE(null, 1, 0),
-    LEVEL(null, 1, 0),
-    SINGLE_ACCUMULATIVE(1, 1, 0),
-    LEVEL_ACCUMULATIVE(1, 1, 0);
+    SINGLE(false,null, 1, 0),
+    SINGLE_ACCUMULATIVE(true,1, 1, 0),
 
+    LEVEL(false, null, 1, 0),
+    LEVEL_ACCUMULATIVE(true,1, 1, 0);
+
+    private final boolean isAccumulative;
     private final Integer initAccumulativeValue;
     private final int initCurrentValue;
     private final Integer initCurrentValueForNextLevel;
 
-    BadgeStrategyType(Integer initAccumulativeValue,
+    BadgeStrategyType(boolean isAccumulative, Integer initAccumulativeValue,
                       int initCurrentValue,
                       Integer initNextLevelCurrentValue) {
+        this.isAccumulative = isAccumulative;
         this.initAccumulativeValue = initAccumulativeValue;
         this.initCurrentValue = initCurrentValue;
         this.initCurrentValueForNextLevel = initNextLevelCurrentValue;
