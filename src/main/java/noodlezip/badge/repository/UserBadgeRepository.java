f3e99596c643d3d8bcd7361311311c29c94d5367
sotogito <sotogitoarchive@gmail.com>
2025-06-27 17:55:51 +0900

    Feat : 라멘 카테고리 레벨 배지 사용자 습득 배지 가져오기 쿼리 작성

--- src/main/java/noodlezip/badge/repository/UserBadgeRepository.java
+++ src/main/java/noodlezip/badge/repository/UserBadgeRepository.java
@@ -24,20 +24,29 @@ public interface UserBadgeRepository extends JpaRepository<UserBadge, Long> {
             Pageable pageable
     );
 
-
     @Query("""
-            select ub
-            from UserBadge ub
-            join fetch ub.badge b
-            where ub.userId = :userId
-            and b.badgeCategory.id = :badgeCategoryId
-            and b.badgeExtraOption.storeSidoLegalCode = :legalCode
-            order by ub.id desc
+        select ub
+        from UserBadge ub
+        join fetch ub.badge b
+        where ub.userId = :userId
+        and b.badgeCategory.id = :badgeCategoryId
+        and b.badgeExtraOption.ramenCategory.id = :ramenCategoryId
+        and b.badgePolicy.badgeLevel = (
+            select max(b2.badgePolicy.badgeLevel)
+            from UserBadge ub2
+            join ub2.badge b2
+            where ub2.userId = :userId
+            and b2.badgeCategory.id = :badgeCategoryId
+            and b2.badgeExtraOption.ramenCategory.id = :ramenCategoryId
+        )
+        order by ub.obtainedAt desc
             """)
-    List<UserBadge> findByUserIdAndBadgeCategoryIdAndLegalCode(
+    List<UserBadge> findByUserIdAndBadgeCategoryIdAndRamenCategoryIdWithBadge(
             long userId,
             long badgeCategoryId,
-            int legalCode
+            int ramenCategoryId,
+            Pageable pageable
     );
+
 
 }
