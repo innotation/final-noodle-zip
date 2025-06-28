a2792c9eb506618db4d33bf1219f304710e04285
sotogito <sotogitoarchive@gmail.com>
2025-06-28 23:29:08 +0900

    Feat : 라멘 카테고리 배지 로직 작성

---
+++ src/main/java/noodlezip/badge/service/process/level/RamenReviewCategoryBadge.java
@@ -0,0 +1,92 @@
+package noodlezip.badge.service.process.level;
+
+import lombok.RequiredArgsConstructor;
+import noodlezip.badge.constants.BadgeStrategyType;
+import noodlezip.badge.constants.LevelBadgeCategoryType;
+import noodlezip.badge.constants.PostStatusType;
+import noodlezip.badge.entity.Badge;
+import noodlezip.badge.entity.UserBadge;
+import noodlezip.badge.exception.BadgeErrorStatus;
+import noodlezip.badge.repository.BadgeCategoryRepository;
+import noodlezip.badge.repository.BadgeRepository;
+import noodlezip.badge.repository.UserBadgeRepository;
+import noodlezip.badge.service.process.level.handler.LevelUpHandler;
+import noodlezip.badge.service.process.level.handler.ValueUpdateHandler;
+import noodlezip.common.exception.CustomException;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Service
+public class RamenReviewCategoryBadge {
+
+    private final static int invalidRamenCategoryId = 7; //기타
+
+    private final BadgeRepository badgeRepository;
+    private final UserBadgeRepository userBadgeRepository;
+    private final ValueUpdateHandler levelValueUpdateHandler;
+    private final LevelUpHandler levelUpHandler;
+
+    @Transactional
+    public void process(Long userId, int ramenCategoryId) {
+        if (!isValidRamenCategory(ramenCategoryId)) {
+            return;
+        }
+        Long badgeCategoryId = LevelBadgeCategoryType.RAMEN_REVIEW_CATEGORY_BADGE.getDbPk();
+        BadgeStrategyType badgeStrategy = BadgeStrategyType.LEVEL_ACCUMULATIVE;
+        Optional<UserBadge> nowUserBadge = userBadgeRepository.findRamenCategoryLevelUserBadge(
+                userId,
+                badgeCategoryId,
+                ramenCategoryId
+        );
+        if (nowUserBadge.isEmpty()) {
+            createInitialUserBadge(userId, badgeCategoryId, ramenCategoryId, badgeStrategy);
+            return;
+        }
+        UserBadge userBadge = nowUserBadge.get();
+        if (!levelValueUpdateHandler.updateLevelAccumulativeTypeBadgeValue(userBadge)) {
+            return;
+        }
+        levelUpHandler.handle(userBadge, badgeStrategy);
+    }
+
+
+    private void createInitialUserBadge(Long userId,
+                                        Long badgeCategoryId,
+                                        int ramenCategoryId,
+                                        BadgeStrategyType badgeStrategy
+    ) {
+        UserBadge newBadge = getInitBadge(userId, badgeCategoryId, ramenCategoryId, badgeStrategy);
+        userBadgeRepository.save(newBadge);
+        if (newBadge.isOverCompletionValue()) {
+            levelUpHandler.handle(newBadge, badgeStrategy);
+        }
+    }
+
+
+    private UserBadge getInitBadge(Long userId,
+                                   Long badgeCategoryId,
+                                   int ramenCategoryId,
+                                   BadgeStrategyType badgeStrategy
+    ) {
+        Badge minLevelBAdge = badgeRepository.findInitRamenCategoryLevelBadge(badgeCategoryId, ramenCategoryId)
+                .orElseThrow(() -> new CustomException(BadgeErrorStatus._NOT_FOUND_BADGE));
+
+        return UserBadge.builder()
+                .userId(userId)
+                .badge(minLevelBAdge)
+                .currentValue(badgeStrategy.getInitCurrentValue())
+                .accumulativeValue(badgeStrategy.getInitAccumulativeValue())
+                .postStatus(PostStatusType.VISIBLE)
+                .build();
+    }
+
+    private boolean isValidRamenCategory(int ramenCategoryId) {
+        return ramenCategoryId != invalidRamenCategoryId;
+    }
+
+}
