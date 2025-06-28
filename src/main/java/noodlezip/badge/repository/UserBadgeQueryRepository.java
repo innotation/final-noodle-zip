260e01e9a7b38fc16bc600c605720504c9b63a32
sotogito <sotogitoarchive@gmail.com>
2025-06-28 15:37:57 +0900

    Feat : UserBadge QueryDSL Repository 생성

---
+++ src/main/java/noodlezip/badge/repository/UserBadgeQueryRepository.java
@@ -0,0 +1,15 @@
+package noodlezip.badge.repository;
+
+import noodlezip.badge.entity.UserBadge;
+
+import java.util.Optional;
+
+public interface UserBadgeQueryRepository {
+
+    Optional<UserBadge> findUserLevelBadge(long userId, long badgeCategoryId);
+
+    Optional<UserBadge> findRamenCategoryLevelUserBadge(long userId,
+                                                        long badgeCategoryId,
+                                                        int ramenCategoryId);
+
+}
---
+++ src/main/java/noodlezip/badge/repository/UserBadgeQueryRepositoryImpl.java
@@ -0,0 +1,61 @@
+package noodlezip.badge.repository;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+import noodlezip.badge.entity.QBadge;
+import noodlezip.badge.entity.QBadgeCategory;
+import noodlezip.badge.entity.QUserBadge;
+import noodlezip.badge.entity.UserBadge;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Repository
+public class UserBadgeQueryRepositoryImpl implements UserBadgeQueryRepository {
+
+    private final JPAQueryFactory queryFactory;
+
+    @Override
+    public Optional<UserBadge> findUserLevelBadge(long userId, long badgeCategoryId) {
+        QUserBadge userBadge = QUserBadge.userBadge;
+        QBadge badge = QBadge.badge;
+
+        return Optional.ofNullable(
+                queryFactory
+                        .selectFrom(userBadge)
+                        .join(userBadge.badge, badge).fetchJoin()
+                        .where(
+                                userBadge.userId.eq(userId),
+                                badge.badgeCategory.id.eq(badgeCategoryId)
+                        )
+                        .orderBy(badge.badgePolicy.badgeLevel.desc())
+                        .limit(1)
+                        .fetchOne()
+        );
+    }
+
+    @Override
+    public Optional<UserBadge> findRamenCategoryLevelUserBadge(long userId,
+                                                               long badgeCategoryId,
+                                                               int ramenCategoryId
+    ) {
+        QUserBadge userBadge = QUserBadge.userBadge;
+        QBadge badge = QBadge.badge;
+
+        return Optional.ofNullable(
+                queryFactory
+                        .selectFrom(userBadge)
+                        .join(userBadge.badge, badge).fetchJoin()
+                        .where(
+                                userBadge.userId.eq(userId),
+                                badge.badgeCategory.id.eq(badgeCategoryId),
+                                badge.badgeExtraOption.ramenCategory.id.eq(ramenCategoryId)
+                        )
+                        .orderBy(badge.badgePolicy.badgeLevel.desc())
+                        .limit(1)
+                        .fetchOne()
+        );Refactor : Badge JPA Repository QueryDSL Repository 인터페이스 상속
+    }
+
+}
