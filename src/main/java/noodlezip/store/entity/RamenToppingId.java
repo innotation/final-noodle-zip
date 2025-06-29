ac3a91cc5f406d7ea579f40c0ed7568a8ed46243
Kyungbae_Go <gokyungbae@gmail.com>
2025-06-30 00:31:19 +0900

    Feature: Entity추가
    - RamenSoup, RamenTopping, RamenToppingId, StoreExtraTopping, Topping 추가
    - RamenTopping은 복합키로 RamenToppungId로 Embadded

--- src/main/java/noodlezip/store/entity/RamenToppingId.java
+++ src/main/java/noodlezip/store/entity/RamenToppingId.java
@@ -1,7 +1,21 @@
 package noodlezip.store.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
 
-@Entity
-public class RamenToppingId {
-  }
+import java.io.Serializable;
+
+@Embeddable
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+public class RamenToppingId implements Serializable {
+    @Column(name="menu_id", nullable = false)
+    private Long menuId;
+    @Column(name="topping_id", nullable = false)
+    private Long toppingId;
+}
--- src/main/java/noodlezip/store/entity/StoreExtraTopping.java
+++ src/main/java/noodlezip/store/entity/StoreExtraTopping.java
@@ -1,7 +1,31 @@
 package noodlezip.store.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 @Entity
+@Table(name = "tbl_store_extra_topping")
 public class StoreExtraTopping {
-  }
+    @Id
+    @Column(name = "store_extra_topping_id", nullable = false)
+    private Long id;
+
+    @NotNull
+    @Column(name = "store_id", nullable = false)
+    private Long storeId;
+
+    @NotNull
+    @Column(name = "topping_id", nullable = false)
+    private Long toppingId;
+
+    @Column(name = "price")
+    private Integer price;
+
+}
