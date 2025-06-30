76728a51a7fcb9fd5f34fc8790e0017d8cc54d3a
Seoyeon <106144694+podoseee@users.noreply.github.com>
2025-06-30 14:00:40 +0900

    Fix: Dto 타입 변경

--- src/main/java/noodlezip/store/dto/MenuRequestDto.java
+++ src/main/java/noodlezip/store/dto/MenuRequestDto.java
@@ -17,7 +17,7 @@ public class MenuRequestDto {
     private String menuImageUrl;
 
     private Long ramenCategoryId; // ramenCategory 엔티티의 ID
-    private Integer ramenSoupId;     // ramenSoup 엔티티의 ID
+    private Long ramenSoupId;     // ramenSoup 엔티티의 ID
 
     private List<Long> defaultToppingIds; // 기본 토핑 ID 목록 (다대다 관계)
 }
--- src/main/java/noodlezip/store/dto/StoreRequestDto.java
+++ src/main/java/noodlezip/store/dto/StoreRequestDto.java
@@ -1,6 +1,9 @@
 package noodlezip.store.dto;
 
 import lombok.*;
+import noodlezip.store.constant.ApprovalStatus;
+import noodlezip.store.constant.OperationStatus;
+import noodlezip.store.constant.ParkingType;
 
 import java.util.List;
 
@@ -17,13 +20,15 @@ public class StoreRequestDto {
 
     private Boolean isLocalCard;      // 지역카드 가능 여부
     private Boolean isChildAllowed;   // 유아동반 가능 여부
-    private String hasParking;        // 무료/유료/불가
+    private ParkingType hasParking;       // 무료/유료/불가 (Enum)
 
     private String ownerComment;      // 사장님 한 줄 소개
-    private String storeMainImageUrl; // 대표 이미지 URL
 
     private Double xAxis; // 위도
     private Double yAxis; // 경도
+
+    private ApprovalStatus approvalStatus;   // 승인 상태 (WAITING, APPROVED) (Enum)
+    private OperationStatus operationStatus;  // 운영 상태 (OPEN, CLOSED) (Enum)
 
     private List<MenuRequestDto> menus; // 메뉴 리스트 포함(메뉴 엔티티로 매핑)
     private List<StoreScheduleRequestDto> weekSchedule;  // 요일별 영업시간
