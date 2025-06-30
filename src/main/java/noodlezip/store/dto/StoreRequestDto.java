fd05c95c0ab94c9b83ffe38748ae5069d0372368
Seoyeon <106144694+podoseee@users.noreply.github.com>
2025-06-30 20:27:25 +0900

    Fix: 이미지저장 url

--- src/main/java/noodlezip/store/dto/StoreRequestDto.java
+++ src/main/java/noodlezip/store/dto/StoreRequestDto.java
@@ -20,7 +20,7 @@ public class StoreRequestDto {
 
     private Boolean isLocalCard;      // 지역카드 가능 여부
     private Boolean isChildAllowed;   // 유아동반 가능 여부
-    private ParkingType hasParking;       // 무료/유료/불가 (Enum)
+    private ParkingType hasParking;   // 무료/유료/불가 (Enum)
 
     private String ownerComment;      // 사장님 한 줄 소개
 
@@ -32,6 +32,8 @@ public class StoreRequestDto {
 
     private List<MenuRequestDto> menus; // 메뉴 리스트 포함(메뉴 엔티티로 매핑)
     private List<StoreScheduleRequestDto> weekSchedule;  // 요일별 영업시간
+
+    private String storeMainImageUrl; // 이미지저장 url
 
     // userId는 서버에서 받아와서 Entity에 매핑해야함
 }
