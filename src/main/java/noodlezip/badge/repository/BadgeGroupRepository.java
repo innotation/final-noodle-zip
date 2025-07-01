a0504e9c99fe67d56b166cec273e7970b150d4fa
sotogito <sotogitoarchive@gmail.com>
2025-07-01 20:01:56 +0900

    Fix : 그룹 조회 jqpl쿼리 dto 경로 수정

--- src/main/java/noodlezip/badge/repository/BadgeGroupRepository.java
+++ src/main/java/noodlezip/badge/repository/BadgeGroupRepository.java
@@ -1,7 +1,7 @@
 package noodlezip.badge.repository;
 
 import noodlezip.badge.entity.BadgeGroup;
-import noodlezip.mypage.dto.BadgeGroupResponse;
+import noodlezip.mypage.dto.response.BadgeGroupResponse;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -9,7 +9,11 @@ import java.util.List;
 
 public interface BadgeGroupRepository extends JpaRepository<BadgeGroup, Long> {
 
-    @Query("select new noodlezip.mypage.dto.BadgeGroupResponse(bg.id, bg.badgeGroupName) from BadgeGroup bg order by bg.id asc")
+    @Query("""
+             select new noodlezip.mypage.dto.response.BadgeGroupResponse(bg.id, bg.badgeGroupName)
+             from BadgeGroup bg
+             order by bg.id asc
+            """)
     List<BadgeGroupResponse> getBadgeGroups();
 
 }
