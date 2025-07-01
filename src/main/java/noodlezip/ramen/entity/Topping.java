a62fbbe2f37430409b5d68f328a5968e3b1c4eea
Seoyeon <106144694+podoseee@users.noreply.github.com>
2025-07-01 16:15:59 +0900

    Fix: Ramen Entity 수정

--- src/main/java/noodlezip/ramen/entity/Topping.java
+++ src/main/java/noodlezip/ramen/entity/Topping.java
@@ -28,6 +28,6 @@ public class Topping {
     @NotNull
     @ColumnDefault("1")
     @Column(name = "is_active", nullable = false)
-    private Boolean isActive = false;
+    private Boolean isActive;
 
 }
