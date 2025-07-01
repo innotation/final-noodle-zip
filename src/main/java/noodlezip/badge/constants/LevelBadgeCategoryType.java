f2377dedc662ab3b17dbe83a42744c913e9588b4
sotogito <sotogitoarchive@gmail.com>
2025-07-01 09:44:55 +0900

    Feat : 배지 카테고리 Enum 옵션 여부 정의

--- src/main/java/noodlezip/badge/constants/LevelBadgeCategoryType.java
+++ src/main/java/noodlezip/badge/constants/LevelBadgeCategoryType.java
@@ -2,23 +2,48 @@ package noodlezip.badge.constants;
 
 import lombok.Getter;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Getter
 public enum LevelBadgeCategoryType {
 
-    REVIEW_GET_LIKE_COUNT_BADGE(1L),
-    COMMUNITY_GET_LIKE_COUNT_BADGE(2L),
+    REVIEW_GET_LIKE_COUNT_BADGE(1L, false),
+    COMMUNITY_GET_LIKE_COUNT_BADGE(2L, false),
 
-    ALL_COMMUNITY_POST_COUNT_BADGE(3L),
-    ALL_COMMENT_POST_COUNT_BADGE(4L),
+    ALL_COMMUNITY_POST_COUNT_BADGE(3L, false),
+    ALL_COMMENT_POST_COUNT_BADGE(4L, false),
 
-    RAMEN_REVIEW_REGION_BADGE(5L),
-    RAMEN_REVIEW_CATEGORY_BADGE(6L);
+    RAMEN_REVIEW_REGION_BADGE(5L, true),
+    RAMEN_REVIEW_CATEGORY_BADGE(6L, true);
 
     private final Long dbPk;
+    private final boolean isOptional;
 
 
-    LevelBadgeCategoryType(Long dbPk) {
+    LevelBadgeCategoryType(Long dbPk, boolean isOptional) {
         this.dbPk = dbPk;
+        this.isOptional = isOptional;
+    }
+
+    public static List<Long> getNotOptionBadgeCategoryIdList() {
+        List<Long> result = new ArrayList<>();
+        for (LevelBadgeCategoryType badgeCategoryType : LevelBadgeCategoryType.values()) {
+            if(!badgeCategoryType.isOptional) {
+                result.add(badgeCategoryType.getDbPk());
+            }
+        }
+        return result;
+    }
+
+    public static List<Long> getOptionBadgeCategoryIdList() {
+        List<Long> result = new ArrayList<>();
+        for (LevelBadgeCategoryType badgeCategoryType : LevelBadgeCategoryType.values()) {
+            if(badgeCategoryType.isOptional) {
+                result.add(badgeCategoryType.getDbPk());
+            }
+        }
+        return result;
     }
 
 }
