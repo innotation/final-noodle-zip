6ce5ccf0386096540482fd075638fef152c8d7f2
sotogito <sotogitoarchive@gmail.com>
2025-07-02 20:51:19 +0900

    Feat : 배지 상세보기 조회 로직 작성

--- src/main/java/noodlezip/mypage/service/MyBadgeService.java
+++ src/main/java/noodlezip/mypage/service/MyBadgeService.java
@@ -1,11 +1,14 @@
 package noodlezip.mypage.service;
 
+import noodlezip.mypage.dto.response.BadgeDetailResponse;
 import noodlezip.mypage.dto.response.MyBadgeBadgeResponse;
 
 import java.util.List;
 
-public interface BadgeService {
+public interface MyBadgeService {
 
     List<MyBadgeBadgeResponse> getUserBadgeListByGroup(Long userId);
+
+    BadgeDetailResponse getBadgeDetailList(Long userId, Long badgeId, Long badgeCategoryId);
 
 }
