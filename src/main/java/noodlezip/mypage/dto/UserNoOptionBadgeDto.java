86a302495102154b779c2b76c2f097ed6e55dfda
sotogito <sotogitoarchive@gmail.com>
2025-07-02 20:50:15 +0900

    Feat : UserNoOptionBadgeDto, UserOptionBadgeDto 내부 Response반환 메서드 삭제
    - 서비스에서 변경

--- src/main/java/noodlezip/mypage/dto/UserNoOptionBadgeDto.java
+++ src/main/java/noodlezip/mypage/dto/UserNoOptionBadgeDto.java
@@ -1,7 +1,6 @@
 package noodlezip.mypage.dto;
 
 import lombok.*;
-import noodlezip.mypage.dto.response.UserBadgeResponse;
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -19,19 +18,5 @@ public class UserNoOptionBadgeDto {
     private String badgeName;
     private Integer accumulativeValue;
     private String badgeImageUrl;
-
-
-    public UserBadgeResponse toUserBadgeResponse() {
-        return UserBadgeResponse.builder()
-                .userBadgeId(userBadgeId)
-                .badgeId(badgeId)
-                .badgeCategoryId(badgeCategoryId)
-                .badgeGroupId(badgeGroupId)
-                .badgeTitleName(badgeCategoryName)
-                .badgeLevelName(badgeName)
-                .accumulativeValue(accumulativeValue)
-                .badgeImageUrl(badgeImageUrl)
-                .build();
-    }
 
 }
--- src/main/java/noodlezip/mypage/dto/UserOptionBadgeDto.java
+++ src/main/java/noodlezip/mypage/dto/UserOptionBadgeDto.java
@@ -1,8 +1,6 @@
 package noodlezip.mypage.dto;
 
 import lombok.*;
-import noodlezip.badge.constants.Region;
-import noodlezip.mypage.dto.response.UserBadgeResponse;
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -21,26 +19,5 @@ public class UserOptionBadgeDto {
     private String badgeName;
     private Integer accumulativeValue;
     private String badgeImageUrl;
-
-
-    public UserBadgeResponse toUserBadgeResponse() {
-        String badgeTitleName = "";
-        if (storeSidoLegalCode == null) {
-            badgeTitleName = ramenCategoryName;
-        } else if (ramenCategoryName == null) {
-            badgeTitleName = Region.getRegionBySidoCode(storeSidoLegalCode).getName();
-        }
-
-        return UserBadgeResponse.builder()
-                .userBadgeId(userBadgeId)
-                .badgeId(badgeId)
-                .badgeCategoryId(badgeCategoryId)
-                .badgeGroupId(badgeGroupId)
-                .badgeTitleName(badgeTitleName)
-                .badgeLevelName(badgeName)
-                .accumulativeValue(accumulativeValue)
-                .badgeImageUrl(badgeImageUrl)
-                .build();
-    }
 
 }
