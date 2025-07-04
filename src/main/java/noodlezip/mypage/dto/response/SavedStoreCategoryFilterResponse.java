d2e1da324b95a0a021d15ffb1b6c7b3f9c2bb39c
sotogito <sotogitoarchive@gmail.com>
2025-07-04 21:29:04 +0900

    Feat : 저장가게 카테고리 조회 필터 SavedStoreCatetogryFilterResponse 생성

---
+++ src/main/java/noodlezip/mypage/dto/response/SavedStoreCategoryFilterResponse.java
@@ -0,0 +1,12 @@
+package noodlezip.mypage.dto.response;
+
+import noodlezip.savedstore.dto.response.SavedStoreCategoryResponse;
+
+import java.util.List;
+
+public class SavedStoreCategoryFilterResponse {
+
+    private List<SavedStoreCategoryResponse> savedStoreCategoryList;
+
+}
+
