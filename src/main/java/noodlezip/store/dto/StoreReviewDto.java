a3d3a3eb1bec6a056a43d21edfe56ffcd0c6f15d
kakaba-hub <gokyungbae@gmail.com>
2025-07-04 17:38:20 +0900

    Fix: RamenReview TYPE 수정
    - RamenReview Entity 수정
    - StoreReviewDto 수정
    - 해당 TestCode 수정

--- src/main/java/noodlezip/store/dto/StoreReviewDto.java
+++ src/main/java/noodlezip/store/dto/StoreReviewDto.java
@@ -16,20 +16,36 @@ public class StoreReviewDto {
     private Long communityId;
     private Long menuId;
     private String menuName;
-    private String noodleThickness;
-    private String noodleTexture;
-    private String noodleBoilLevel;
-    private String soupTemperature;
-    private String soupSaltiness;
-    private String soupSpicinessLevel;
-    private String soupOiliness;
+    private Integer noodleThickness;
+    private Integer noodleTexture;
+    private Integer noodleBoilLevel;
+    private Integer soupTemperature;
+    private Integer soupSaltiness;
+    private Integer soupSpicinessLevel;
+    private Integer soupOiliness;
     private String soupFlavorKeywords;
     private String content;
     private String reviewImageUrl;
     private Boolean isReceiptReview;
     private List<String> toppingNames;
 
-    public StoreReviewDto(Long id, Long communityId, Long menuId, String menuName, String noodleThickness, String noodleTexture, String noodleBoilLevel, String soupTemperature, String soupSaltiness, String soupSpiciness, String soupOiliness, String soupFlavorKeywords, String content, String reviewImageUrl, Boolean isReceiptReview) {
+    public StoreReviewDto(
+            Long id,
+            Long communityId,
+            Long menuId,
+            String menuName,
+            Integer noodleThickness,
+            Integer noodleTexture,
+            Integer noodleBoilLevel,
+            Integer soupTemperature,
+            Integer soupSaltiness,
+            Integer soupSpiciness,
+            Integer soupOiliness,
+            String soupFlavorKeywords,
+            String content,
+            String reviewImageUrl,
+            Boolean isReceiptReview
+    ) {
         this.id = id;
         this.communityId = communityId;
         this.menuId = menuId;
