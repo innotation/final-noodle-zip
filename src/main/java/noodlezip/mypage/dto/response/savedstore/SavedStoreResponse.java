87e40495d616f626149a7d6a5361a3a37adb5640
sotogito <sotogitoarchive@gmail.com>
2025-07-06 03:12:28 +0900

    Feat : 저장 가게 디테일 SavedStoreResponse dto 생성

---
+++ src/main/java/noodlezip/mypage/dto/response/savedstore/SavedStoreResponse.java
@@ -0,0 +1,13 @@
+package noodlezip.mypage.dto.response.savedstore;
+
+public class SavedStoreResponse {
+
+    private Long savedStoreId;
+    private Long saveStoreCategoryId;
+    private Long storeId;
+    private String storeName;
+    private String address;
+    private String storeMainImageUrl;
+    private String memo;
+
+}
