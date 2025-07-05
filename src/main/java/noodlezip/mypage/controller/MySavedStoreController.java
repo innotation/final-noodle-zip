acc1bb34b1e0ce38f1f9ef87993bf2d94bfdbc88
sotogito <sotogitoarchive@gmail.com>
2025-07-06 03:08:06 +0900

    Feat : MySavedStoreController 작성
    - 본인 저장가게 페이지 반환
    - 타인 저장가게 페이지 반환
    - 저장가게 필터링 검색 및 페이지네이션 비동기처리
    - 지도보기에 띄울 저장가게 좌표값 반환 비동기 처리

--- src/main/java/noodlezip/mypage/controller/MySavedStoreController.java
+++ src/main/java/noodlezip/mypage/controller/MySavedStoreController.java
@@ -3,10 +3,13 @@ package noodlezip.mypage.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import noodlezip.common.auth.MyUserDetails;
-import noodlezip.common.exception.CustomException;
-import noodlezip.mypage.dto.response.SavedStoreCategoryFilterResponse;
-import noodlezip.mypage.exception.MyPageErrorStatus;
-import noodlezip.savedstore.dto.response.SavedStoreCategoryResponse;
+import noodlezip.mypage.dto.request.savedstore.SavedStoreCategoryFilterRequest;
+import noodlezip.mypage.dto.response.savedstore.MySavedStorePageResponse;
+import noodlezip.mypage.dto.response.savedstore.SavedStoreListResponse;
+import noodlezip.mypage.dto.response.savedstore.SavedStorePageResponse;
+import noodlezip.mypage.dto.response.savedstore.StoreLocationResponse;
+import noodlezip.mypage.util.SavedStorePagePolicy;
+import noodlezip.savedstore.service.SavedStoreService;
 import noodlezip.user.entity.User;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
@@ -20,70 +23,66 @@ import java.util.List;
 @RequestMapping("/mypage")
 @Controller
 public class MySavedStoreController {
-    /**
-     * 저장 가게 카테고리 목록 조회 - userId
-     * 저장가게전체 조회            - userId(페이지네이션)
-     * 선택 카테고리 조회           - userId + categoryId(페이지네이션)
-     * 좌표 조회                    - userId + categoryId
-     * 선택좌표상세보기조회(경배님이만든거) - storeId (근데 메모 띄우려면 savedStoreId에서 store조인헤야겠ㄸ니
-     */
+
+    private final SavedStoreService savedStoreService;
 
     @GetMapping("/my/saved-store/list")
     /// 사용자가 본인의 마이페이지에 접근해야만 들어올 수 있음
     public String mySavedStoreList(@AuthenticationPrincipal MyUserDetails userDetails, Model model) {
         User user = userDetails.getUser();
+        MySavedStorePageResponse mySavedStorePageInfo = savedStoreService.getMySavedStoreInitPage(user.getId());
 
-        /**
-         * 처음에 조회할때떄는
-         * 1. 카테고리 목록 필터 - SavedStoreCategoryResponse
-         * 2. 조회 결과 페이지네이션 결과 - 페이지네이션정보 + List\<저장가게정보>
-         *
-         *     카테고리 목록 조회는 좌표 찍을떄도 필요하기 때문에 따로 보내는게 좋은 거 같다.(지도보기 누르면 SavedStoreCategoryResponse 보내야됨)
-         */
         model.addAttribute("userId", user.getId());
         model.addAttribute("isOwner", true);
+        model.addAttribute("mySavedStorePageInfo", mySavedStorePageInfo);
+
         return "index";
     }
 
     @GetMapping("/{userId}/saved-store/list")
     public String savedStoreList(@PathVariable Long userId, Model model) {
+        SavedStorePageResponse savedStorePageInfo = savedStoreService.getSavedStoreInitPage(userId);
 
         model.addAttribute("userId", userId);
         model.addAttribute("isOwner", false);
+        model.addAttribute("savedStorePageInfo", savedStorePageInfo);
+
         return "index";
     }
 
 
-    /// 이때는 사용자가 검색 기능을 눌렀을때 + 처음 조회 페이지에서 페이지 넘겼을 때 ㅍ리터 비동기 조회
-    @GetMapping("/{userId}/saved-store/list/category-filter-search") //내 조회, 다른사람 조회 같이 사용 - 어차피 비동기
+    /// 카테고리 조회 + 페이지네이션
+    @GetMapping("/{userId}/saved-store/list/category-filter-search")
     @ResponseBody
-    public void getMySavedStoreListByCategory(@AuthenticationPrincipal MyUserDetails userDetails,
-                                              @PathVariable Long userId,
-                                              @ModelAttribute SavedStoreCategoryFilterResponse categoryFilter
+    public SavedStoreListResponse getMySavedStoreListByCategory(
+            @AuthenticationPrincipal MyUserDetails userDetails,
+            @PathVariable Long userId,
+            @ModelAttribute SavedStoreCategoryFilterRequest filter,
+            @RequestParam(defaultValue = SavedStorePagePolicy.DEFAULT_PAGE) int page
     ) {
-
         User user = userDetails.getUser();
-        if(!user.getId().equals(userId)) {
-            throw new CustomException(MyPageErrorStatus._NOT_FOUND_USER_MY_PAGE);
-        }
+        boolean isOwner = user.getId().equals(userId);
+
+        return savedStoreService.getSavedStoreListWithPaging(
+                userId, filter, page, isOwner
+        );
     }
-    
-    
+
+
     /// 사용자가 지도보기를 눌렀을때  storeId+좌표 리스트 비동기 조회
     @GetMapping("/{userId}/saved-store/list/map")
     @ResponseBody
-    public void getMySavedStoreListMap(@AuthenticationPrincipal MyUserDetails userDetails,
-                                       @PathVariable Long userId,
-                                       @ModelAttribute SavedStoreCategoryFilterResponse categoryFilter
+    public List<StoreLocationResponse> getMySavedStoreListMap(@AuthenticationPrincipal MyUserDetails userDetails,
+                                                              @PathVariable Long userId,
+                                                              @ModelAttribute SavedStoreCategoryFilterRequest filter
     ) {
         User user = userDetails.getUser();
-        if(!user.getId().equals(userId)) {
-            throw new CustomException(MyPageErrorStatus._NOT_FOUND_USER_MY_PAGE);
-        }
-    }
-    
-    
-    
+        boolean isOwner = user.getId().equals(userId);
 
+        return savedStoreService.getStoreLocationList(
+                userId, filter, isOwner
+        );
+    }
 
 }
+
