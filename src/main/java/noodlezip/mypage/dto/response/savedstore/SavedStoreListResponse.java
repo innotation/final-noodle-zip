39f00143cbe43957ba8984c44a99026f709be61f
sotogito <sotogitoarchive@gmail.com>
2025-07-06 03:11:51 +0900

    Feat : 페이지네이션 적용 저장가게 리스트 반환 SavedStoreListResponse dto 생성

---
+++ src/main/java/noodlezip/mypage/dto/response/savedstore/SavedStoreListResponse.java
@@ -0,0 +1,36 @@
+package noodlezip.mypage.dto.response.savedstore;
+
+import lombok.*;
+
+import java.util.List;
+import java.util.Map;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+public class SavedStoreListResponse {
+
+    private int page;
+    private int totalPage;
+    private int startPage;
+    private int beginPage;
+    private boolean isFirst;
+    private boolean isLast;
+
+    private List<SavedStoreResponse> savedStoreList;
+
+
+    public static SavedStoreListResponse of(Map<String, Object> map) {
+        return SavedStoreListResponse.builder()
+                .page((Integer) map.get("page"))
+                .totalPage((Integer) map.get("totalPage"))
+                .startPage((Integer) map.get("startPage"))
+                .beginPage((Integer) map.get("beginPage"))
+                .isFirst((Boolean) map.get("isFirst"))
+                .isLast((Boolean) map.get("isLast"))
+                .build();
+    }
+    
+}
