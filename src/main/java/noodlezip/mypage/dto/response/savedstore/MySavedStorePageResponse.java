5a94ae6c8c8a1127b6411da304cae82f5376c65b
sotogito <sotogitoarchive@gmail.com>
2025-07-06 03:10:30 +0900

    Feat : 본인 저장가게 페이지 MySavedStorePageResponse dto생성
    - 검색용 카테고리 리스트
    - 카테고리 수정용 카테고리 리스트
    - 페이지네이션 적용 저장가게 리스트

---
+++ src/main/java/noodlezip/mypage/dto/response/savedstore/MySavedStorePageResponse.java
@@ -0,0 +1,19 @@
+package noodlezip.mypage.dto.response.savedstore;
+
+import lombok.*;
+import noodlezip.savedstore.dto.response.SavedStoreCategoryResponse;
+
+import java.util.List;
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+public class MySavedStorePageResponse {
+
+    /// my, {userId}
+    private List<SavedStoreCategoryResponse> searchFilter;
+    private List<SavedStoreCategoryResponse> updateCategoryList;
+    private SavedStoreListResponse savedStoreList;
+
+}
