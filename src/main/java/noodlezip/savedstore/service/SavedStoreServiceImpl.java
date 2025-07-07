875b41e1723b98a32bf4b30a9716f87b314817e1
sotogito <sotogitoarchive@gmail.com>
2025-07-07 12:36:38 +0900

    Comment : 가게상세 협업 주석 수정

--- src/main/java/noodlezip/savedstore/service/SavedStoreServiceImpl.java
+++ src/main/java/noodlezip/savedstore/service/SavedStoreServiceImpl.java
@@ -37,14 +37,14 @@ public class SavedStoreServiceImpl implements SavedStoreService {
     private final StoreRepository storeRepository;
     private final PageUtil pageUtil;
 
-    /// todo [가게상세-1] 가게 상세페이지 wishlist 활성화 여부 판단
+    /// todo \[가게상세-1] 가게 상세페이지 wishlist 활성화 여부 판단
     @Override
     @Transactional(readOnly = true)
     public boolean isSavedStore(Long userId, Long storeId) {
         return saveStoreRepository.existsByUserIdAndStoreId(userId, storeId);
     }
 
-    /// todo [가게상세-2] wishlist 클릭 후 모달에 띄워질 사용자 가게저장카테고리 리스트
+    /// todo \[가게상세-2] wishlist 클릭 후 모달에 띄워질 사용자 가게저장카테고리 리스트
     @Override
     @Transactional(readOnly = true)
     public List<SavedStoreCategoryResponse> getUserSaveCategoryList(Long userId, Long storeId) {
@@ -79,7 +79,7 @@ public class SavedStoreServiceImpl implements SavedStoreService {
     }
 
 
-    /// todo [가게상세-3] 가게 저장 및 수정 - 모달 '저장' 버튼 클릭시
+    /// todo \[가게상세-3] 가게 저장 및 수정 - 모달 '저장' 버튼 클릭시
     @Override
     @Transactional
     public void saveSavedStore(Long userId, Long storeId, SaveStoreRequest saveStoreRequest) {
@@ -150,7 +150,7 @@ public class SavedStoreServiceImpl implements SavedStoreService {
     }
 
 
-    /// todo [가게상세-4] 가게저장 리스트에 삭제 - 모달 '삭제' 버튼 클릭시
+    /// todo \[가게상세-4] 가게저장 리스트에 삭제 - 모달 '삭제' 버튼 클릭시
     @Override
     @Transactional
     public void deleteSavedStore(Long userId, Long storeId) {
