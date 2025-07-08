5bb2945a88d7274e31bc8b970ad1a48287fefd8f
sotogito <sotogitoarchive@gmail.com>
2025-07-08 17:20:22 +0900

    Feat : SaedStoreResponse 위도 경도 추가 및 생성자 오버로드

--- src/main/java/noodlezip/mypage/dto/response/savedstore/SavedStoreResponse.java
+++ src/main/java/noodlezip/mypage/dto/response/savedstore/SavedStoreResponse.java
@@ -3,7 +3,6 @@ package noodlezip.mypage.dto.response.savedstore;
 import lombok.*;
 
 @NoArgsConstructor
-@AllArgsConstructor
 @Getter
 @Setter
 @ToString
@@ -17,5 +16,47 @@ public class SavedStoreResponse {
     private String address;
     private String storeMainImageUrl;
     private String memo;
+
+    private Double storeLat;
+    private Double storeLng;
+
+
+    public SavedStoreResponse(Long savedStoreId,
+                              Long saveStoreCategoryId,
+                              Long storeId,
+                              String storeName,
+                              String address,
+                              String storeMainImageUrl,
+                              String memo,
+                              Double storeLat,
+                              Double storeLng
+    ) {
+        this.savedStoreId = savedStoreId;
+        this.saveStoreCategoryId = saveStoreCategoryId;
+        this.storeId = storeId;
+        this.storeName = storeName;
+        this.address = address;
+        this.storeMainImageUrl = storeMainImageUrl;
+        this.memo = memo;
+        this.storeLat = storeLat;
+        this.storeLng = storeLng;
+    }
+
+    public SavedStoreResponse(Long savedStoreId,
+                              Long saveStoreCategoryId,
+                              Long storeId,
+                              String address,
+                              String storeName,
+                              String storeMainImageUrl,
+                              String memo
+    ) {
+        this.savedStoreId = savedStoreId;
+        this.saveStoreCategoryId = saveStoreCategoryId;
+        this.storeId = storeId;
+        this.address = address;
+        this.storeName = storeName;
+        this.storeMainImageUrl = storeMainImageUrl;
+        this.memo = memo;
+    }
 
 }
