760967ba742d06b627d4b1e828bdea812d8e10ba
inhomun <inho8063@icloud.com>
2025-07-09 17:21:44 +0900

    Feat: 최근 본 게시글 기능 구현

--- src/main/java/noodlezip/community/service/BoardService.java
+++ src/main/java/noodlezip/community/service/BoardService.java
@@ -3,11 +3,13 @@ package noodlezip.community.service;
 
 import noodlezip.community.dto.BoardReqDto;
 import noodlezip.community.dto.BoardRespDto;
+import noodlezip.community.entity.Board;
 import noodlezip.community.entity.BoardUserId;
 import noodlezip.user.entity.User;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.List;
 import java.util.Map;
 
 public interface BoardService {
@@ -18,4 +20,5 @@ public interface BoardService {
     void deleteBoard(Long boardId, Long userId);
     boolean toggleLike(BoardUserId boardUserId);
     Integer getLikeCount(Long boardId);
+    List<Board> getBoardsByIds(List<Long> recentBoardIds);
 }
