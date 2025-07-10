00a8b14cf1df885939ca0d22fb216e384ff1c647
sotogito <sotogitoarchive@gmail.com>
2025-07-10 19:16:40 +0900

    Feat : 나를 팔로우한 사람 페이지네이션 조회 FollowerPageResponse 생성

---
+++ src/main/java/noodlezip/subscription/dto/response/FollowerPageResponse.java
@@ -0,0 +1,37 @@
+package noodlezip.subscription.dto.response;
+
+import lombok.*;
+
+import java.util.List;
+import java.util.Map;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@Builder
+public class FollowerPageResponse {
+
+    private int page;
+    private int totalPage;
+    private int beginPage;
+    private int endPage;
+    private boolean isFirst;
+    private boolean isLast;
+
+    private List<FollowerResponse> followerList;
+
+
+    public static FollowerPageResponse of(Map<String, Object> map) {
+        return FollowerPageResponse.builder()
+                .page((Integer) map.get("page"))
+                .totalPage((Integer) map.get("totalPage"))
+                .beginPage((Integer) map.get("beginPage"))
+                .endPage((Integer) map.get("endPage"))
+                .isFirst((Boolean) map.get("isFirst"))
+                .isLast((Boolean) map.get("isLast"))
+                .build();
+    }
+
+}
