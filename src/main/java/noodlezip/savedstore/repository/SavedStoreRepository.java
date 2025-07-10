d1d59d67e07cc75f0899cd273850c36f3fae69b1
sotogito <sotogitoarchive@gmail.com>
2025-07-10 14:38:22 +0900

    Feat : 가게저장시 메모 반환 레포 작성

--- src/main/java/noodlezip/savedstore/repository/SavedStoreRepository.java
+++ src/main/java/noodlezip/savedstore/repository/SavedStoreRepository.java
@@ -6,15 +6,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 import java.util.Optional;
 
-public interface SavedStoreRepository extends JpaRepository<SavedStore, Long> , SaveStoreQueryRepository{
+public interface SavedStoreRepository extends JpaRepository<SavedStore, Long>, SaveStoreQueryRepository {
 
     void deleteByUserIdAndStoreId(long userId, long storeId);
 
     boolean existsByUserIdAndStoreId(long userId, long storeId);
 
-    Optional<SavedStore> findByUserIdAndStoreId(long userId, long storeId);
+    Optional<SavedStore> findFirstByUserIdAndStoreId(long userId, long storeId);
 
-    // 여러 결과를 반환하는 메서드 추가
     List<SavedStore> findAllByUserIdAndStoreId(long userId, long storeId);
 
 }
