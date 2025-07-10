901c47b0994c8623411612c3f4185c1c54e7b100
sotogito <sotogitoarchive@gmail.com>
2025-07-10 19:25:58 +0900

    Feat : UserSubscriptionRepository 생성

---
+++ src/main/java/noodlezip/subscription/repository/UserSubscriptionRepository.java
@@ -0,0 +1,12 @@
+package noodlezip.subscription.repository;
+
+import noodlezip.subscription.entity.UserSubscription;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserSubscriptionRepository extends JpaRepository<UserSubscription, Long>, UserSubscriptionQueryRepository {
+
+    boolean existsByFollowerIdAndFolloweeId(long followerId, long followeeId);
+
+    void deleteByFollowerIdAndFolloweeId(long followerId, long followeeId);
+
+}
