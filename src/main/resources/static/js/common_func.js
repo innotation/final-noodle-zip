2ed6076a54001945e709cb08124a594c35b3a4aa
Kyungbae_Go <gokyungbae@gmail.com>
2025-07-12 01:07:25 +0900

    Feature: 로그인로그아웃 처리 개선 및 공통 로그인 모달 시스템 구축
    - 로그인,로그아웃 redirectUrl 타임리프 구문 null 문제로 js 방식으로 변경
    - 공통 로그인 모달 시스템 구축
       - 로그인이 필요한 기능 버튼에 login-required 클래스 주입으로
       로그인 체크하여 로그인 모달 띄움
    - detail 페이지에 적용

--- src/main/resources/static/js/common_func.js
+++ src/main/resources/static/js/common_func.js
@@ -173,14 +173,43 @@
         });
     });
 
-    // Like Icon
-    $('.btn_hero.wishlist').on('click', function (e) {
+    // 공통 로그인 모달 호출 함수
+    function openLoginModal() {
+        $.magnificPopup.open({
+            items: {
+                src: '#sign-in-dialog',
+                type: 'inline'
+            },
+            closeBtnInside: true,
+            mainClass: 'my-mfp-zoom-in',
+            callbacks: {
+                open: function() {
+                    var url = window.location.pathname + window.location.search + window.location.hash;
+                    $('#redirectUrlInput').val(url);
+                }
+            }
+        });
+    }
+
+    // 로그인이 필요한 모든 버튼에 공통 적용
+    $('.login-required').on('click', function(e) {
         e.preventDefault();
-        $(this).toggleClass('liked');
+        
+        // 로그인 상태 체크 (Spring Security의 인증 정보 활용)
+        var isLoggedIn = $('body').hasClass('logged-in') || 
+                         $('.user .dropdown').length > 0; // 로그인된 경우 나타나는 요소들
+        
+        if (!isLoggedIn) {
+            openLoginModal();
+        } else {
+            // 로그인된 경우 원래 기능 수행
+            // 위시리스트 추가 로직 등
+            console.log('로그인된 상태 - 원래 기능 수행');
+        }
     });
 
     // Modal Sign In
-    $('#sign-in').magnificPopup({
+    $('.sign-in').magnificPopup({
         type: 'inline',
         fixedContentPos: true,
         fixedBgPos: true,
@@ -190,7 +219,13 @@
         midClick: true,
         removalDelay: 300,
         closeMarkup: '<button title="%title%" type="button" class="mfp-close"></button>',
-        mainClass: 'my-mfp-zoom-in'
+        mainClass: 'my-mfp-zoom-in',
+        callbacks: {
+            open: function() { // 로그인 시 마지막 접속 url 저장
+                var url = window.location.pathname + window.location.search + window.location.hash;
+                $('#redirectUrlInput').val(url);
+            }
+        }
     });
 
     // Show hide password
@@ -216,5 +251,14 @@
             .find(".indicator")
             .toggleClass('icon_minus-06 icon_plus');
     }
+
+    // 로그아웃 링크에 현재 URL을 redirectUrl로 동적으로 세팅
+    document.addEventListener("DOMContentLoaded", function() {
+      var logoutLinks = document.querySelectorAll('.logout-link');
+      var currentUrl = window.location.pathname + window.location.search + window.location.hash;
+      logoutLinks.forEach(function(link) {
+        link.setAttribute('href', '/logout?redirectUrl=' + encodeURIComponent(currentUrl));
+      });
+    });
 
 })(window.jQuery);
