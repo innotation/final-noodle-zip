a20cb08eddc8cc96da9e793958d7328acb0756b5
sotogito <sotogitoarchive@gmail.com>
2025-07-12 16:26:51 +0900

    Feat : UserAcessInfo 필드 추가
    - 조회 당하는 사람
    - 조회 하는 사람
    - 본인 페이지 여부

--- src/main/java/noodlezip/mypage/dto/UserAccessInfo.java
+++ src/main/java/noodlezip/mypage/dto/UserAccessInfo.java
@@ -1,5 +1,17 @@
 package noodlezip.mypage.dto;
 
+import lombok.*;
 
-public record UserAccessInfo(Long targetUserId, boolean isOwner) {
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+@ToString
+public class UserAccessInfo {
+
+    private Long targetUserId;   /// 조회 당하는 사람
+    private Long requestUserId;  /// 조회 하는 사람
+    private Boolean isOwner;     /// 조회 하는 사람 자신의 페이지인지에 대한 여부 (조회 당하는 사람 = 조회 하는 사람)
+
 }
