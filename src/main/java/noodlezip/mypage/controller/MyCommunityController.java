85d6185fc7c782c341fc6843f76a327a62ccf43a
sotogito <sotogitoarchive@gmail.com>
2025-07-13 03:10:08 +0900

    Docs : MyCommunityController swagger 작성

--- src/main/java/noodlezip/mypage/controller/MyCommunityController.java
+++ src/main/java/noodlezip/mypage/controller/MyCommunityController.java
@@ -1,5 +1,9 @@
 package noodlezip.mypage.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import noodlezip.common.auth.MyUserDetails;
@@ -21,11 +25,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequiredArgsConstructor
 @RequestMapping("/users")
 @Controller
+@Tag(name = "마이페이지", description = "미이페이지 연동 API")
 public class MyCommunityController extends MyBaseController {
 
     private final MyCommunityService myCommunityService;
 
 
+    @Operation(
+            summary = "사용자가 작성한 게시글 목록 조회",
+            description = "사용자 마이페이지에서 해당 사용자가 작성한 게시글을 페이지 단위로 조회합니다."
+    )
+    @Parameters({
+            @Parameter(name = "userId", description = "조회 대상 사용자 ID", required = true, example = "5"),
+    })
     @GetMapping("/{userId}/boards")
     public String getMyPostBoards(
             @AuthenticationPrincipal MyUserDetails myUserDetails,
@@ -44,6 +56,13 @@ public class MyCommunityController extends MyBaseController {
     }
 
 
+    @Operation(
+            summary = "사용자가 좋아요한 게시글 목록 조회",
+            description = "사용자 마이페이지에서 해당 사용자가 좋아요한 게시글을 페이지 단위로 조회합니다."
+    )
+    @Parameters({
+            @Parameter(name = "userId", description = "조회 대상 사용자 ID", required = true, example = "5"),
+    })
     @GetMapping("/{userId}/liked-boards")
     public String getMyLikedBoards(
             @AuthenticationPrincipal MyUserDetails myUserDetails,
@@ -62,6 +81,13 @@ public class MyCommunityController extends MyBaseController {
     }
 
 
+    @Operation(
+            summary = "사용자가 작성한 댓글 목록 조회",
+            description = "사용자 마이페이지에서 해당 사용자가 작성한 댓글을 페이지 단위로 조회합니다."
+    )
+    @Parameters({
+            @Parameter(name = "userId", description = "조회 대상 사용자 ID", required = true, example = "5"),
+    })
     @GetMapping("{userId}/comments")
     public String getMyComments(
             @AuthenticationPrincipal MyUserDetails myUserDetails,
