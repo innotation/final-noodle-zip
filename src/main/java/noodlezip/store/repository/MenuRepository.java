e03419900d8d1b26bf0ba6b78255a84e9f80505a
sotogito <sotogitoarchive@gmail.com>
2025-07-16 12:25:50 +0900

    Feat : MenuRepository 메뉴 아이디 리스트로 라멘 카테고리 아이디 리스트 반환 JQPL 작성

--- src/main/java/noodlezip/store/repository/MenuRepository.java
+++ src/main/java/noodlezip/store/repository/MenuRepository.java
@@ -1,8 +1,11 @@
 package noodlezip.store.repository;
 
+import jakarta.persistence.criteria.CriteriaBuilder;
 import noodlezip.store.entity.Menu;
 import noodlezip.store.entity.Store;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,5 +14,10 @@ import java.util.List;
 public interface MenuRepository extends JpaRepository<Menu, Long>, MenuRepositoryCustom {
 
     List<Menu> findMenuIdByStoreId(Store storeId);
+
     boolean existsByStoreIdAndMenuName(Long storeId, String menuName);
+
+    @Query("SELECT m.category.id FROM Menu m WHERE m.id IN :menuIds")
+    List<Integer> findAllCategoryIdByMenuIds(List<Long> menuIds);
+
 }
