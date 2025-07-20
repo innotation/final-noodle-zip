bf01c9a54ac52fb54fe17fdcf1f9538a1c4ee841
ssb7779 <ssb7779@naver.com>
2025-07-21 02:07:29 +0900

    Fix : 리뷰 게시글 내용과 제목이 작성되지 않아도 제출되던 문제 수정

--- src/main/java/noodlezip/community/dto/ReviewReqDto.java
+++ src/main/java/noodlezip/community/dto/ReviewReqDto.java
@@ -1,6 +1,11 @@
 package noodlezip.community.dto;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.*;
+import noodlezip.common.validation.ValidationGroups;
 
 import java.util.List;
 
@@ -12,13 +17,29 @@ import java.util.List;
 @Builder
 public class ReviewReqDto {
 
+    @NotNull
     private Long storeId;
+
+    @NotNull(message = "제목은 비어있을 수 없습니다")
+    @NotBlank(message = "제목은 비어있을 수 없습니다")
     private String title;
+
+    @NotNull(message = "내용은 비어있을 수 없습니다")
+    @NotBlank(message = "내용은 비어있을 수 없습니다")
     private String content;
+
+    @NotBlank
     private String ocrKeyHash;
+
+    @NotBlank
     private String visitDate;
+
+    @NotNull
+    @Size(min = 1, message = "리뷰는 최소 하나 이상 작성해야 합니다.")
+    @Valid
+    private List<MenuReviewDto> reviews;
+
     private Boolean isReceiptReview;
     private String imageUrl;
-    private List<MenuReviewDto> reviews;
 
 }
