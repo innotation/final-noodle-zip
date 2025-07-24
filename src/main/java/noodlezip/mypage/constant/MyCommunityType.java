af20f92ae8b5d9965ea4c9a9f02bc542ff74ba9a
sotogito <sotogitoarchive@gmail.com>
2025-07-25 00:10:02 +0900

    Feat : 마이페이지 나의 커뮤니티 활동 displayname 상수화

--- src/main/java/noodlezip/mypage/constant/MyCommunityType.java
+++ src/main/java/noodlezip/mypage/constant/MyCommunityType.java
@@ -7,10 +7,11 @@ import lombok.Getter;
 @Getter
 public enum MyCommunityType {
 
-    POST_BOARD("boards"),
-    LIKED_BOARD("liked-boards"),
-    POST_COMMENT("comments");
+    POST_BOARD("boards", "내가 작성한 게시글"),
+    LIKED_BOARD("liked-boards", "내가 좋아요한 게시글"),
+    POST_COMMENT("comments","내가 작성한 댓글");
 
     private final String path;
+    private final String displayName;
 
 }
